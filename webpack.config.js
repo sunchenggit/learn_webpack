96922c21ff73d4382dbda1bba528b5f27306e2d8
suncheng <157600408@qq.com>
2021-02-16 06:49:10 +0800

    处理json模块

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 // 导入nodejs中处理路径的包
 const path = require('path')
+const toml = require('toml')
+const yaml = require('yamljs')
+const json5 = require('json5')
 
 module.exports = {
     // 入口
@@ -36,6 +39,27 @@ module.exports = {
         {
           test: /\.xml$/i,
           use: ['xml-loader']
+        },
+        {
+          test: /\.toml$/i,
+          type: 'json',
+          parser: {
+            parse: toml.parse
+          }
+        },
+        {
+          test: /\.yaml$/i,
+          type: 'json',
+          parser: {
+            parse: yaml.parse
+          }
+        },
+        {
+          test: /\.json5$/i,
+          type: 'json',
+          parser: {
+            parse: json5.parse
+          }
         }
       ]
     }
