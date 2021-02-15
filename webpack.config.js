a40ec9edd920dafe91f23ffae7704b5fe1b499ae
suncheng <157600408@qq.com>
2021-02-15 08:19:33 +0800

    起步

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// 导入nodejs中处理路径的包
+const path = require('path')
+
+module.exports = {
+    // 入口
+    entry: './src/index.js',
+    // 出口
+    output: {
+        // 输出路径
+        path: path.resolve(__dirname, 'dist'),
+        // 输出文件名
+        filename: 'bundle.js'
+    }
+}
